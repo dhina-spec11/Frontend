06ca79386e1633d52689ee832c598e971cdbb221
dhina <dhinakarthikeyan2006@gmail.com>
2026-06-24 15:06:36 +0530

    Update API_URL to use environment variable

--- src/firebase.js
+++ src/firebase.js
@@ -42,7 +42,7 @@ if (isFirebaseConfigured) {
 }
 
 // Simulated LocalStorage / MySQL Database API Helper
-const API_URL = 'http://localhost:5000/api';
+const API_URL = import.meta.env.VITE_API_URL;
 
 const mockAuth = {
   signIn: async (email, password) => {
